f2d7c1c9d43c78935f633e3503bec446cd7d0ff9
vega jafri <giantwhitedeer@gmail.com>
2025-10-15 21:25:58 -0400

    additions

--- systems-prototype-03/Assets/Scenes/PentacleLogic.cs
+++ systems-prototype-03/Assets/Scenes/PentacleLogic.cs
@@ -6,24 +6,21 @@ using System.Collections;
 
 public class PentacleLogic : MonoBehaviour
 {
-    public TextMeshProUGUI timerText;
-    private float juggleTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        juggleTime = 0f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        juggleTime += Time.deltaTime;
-        UpdateTimerUI();
+    
     }
 
     public void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("ballBounds"))
+        if (col.gameObject.CompareTag("ball01") && col.gameObject.CompareTag("ball02"))
         {
             GameOver();
         }
@@ -32,12 +29,5 @@ public class PentacleLogic : MonoBehaviour
     public void GameOver()
     {
 
-    }
-    
-    public void UpdateTimerUI()
-    {
-        int minutes = Mathf.FloorToInt(juggleTime / 60);
-        int seconds = Mathf.FloorToInt(juggleTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
